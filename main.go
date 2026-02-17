ecf4aa0e8fe565d20bf18ca1a8b4a398a5129ff7
Maycdouglas <mayconcruzeiroraposa@gmail.com>
2026-02-17 14:18:14 -0300

    Add main.go

---
+++ main.go
@@ -0,0 +1,8 @@
+package main // 1. Declaração obrigatória do pacote
+
+import "fmt" // 2. Importação de pacotes (format)
+
+// 3. Função principal (Entry Point)
+func main() {
+    fmt.Println("Hello World")
+}
